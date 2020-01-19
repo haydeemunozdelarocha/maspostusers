f04779b18a8cc73a5f9567142b80c24675e6b561
haydeemunoz <haydeemunozdelarocha@gmail.com>
2020-01-19 14:55:39 +0100

    removed isSelected column from showing in card

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,7 +4,7 @@ import {formatColumnNameToLabel} from "../helpers/formatting";
 
 class CardList extends React.Component {
     render() {
-        const {onCheck, selectAll, cards, allSelected} = this.props;
+        const {onCheck,selectAll, cards, allSelected} = this.props;
         return (
             <div>
                 <button className="btn btn-secondary" style={{marginBottom: '20px', float: 'rog '}} onClick={(e) => selectAll()}>{allSelected ? 'Unselect All' : 'Select All'}</button>
@@ -13,7 +13,7 @@ class CardList extends React.Component {
                     return (<div className="panel-contrast card">
                         <input type="checkbox" checked={!!card.isSelected} onClick={(e) => onCheck(card)}/>
                         {
-                            cardColumns.map((column) => (<p><span className="card-line-label">{formatColumnNameToLabel(column)}: </span>{card[column]}</p>))
+                            cardColumns.map((column) => (column !== 'isSelected' && <p><span className="card-line-label">{formatColumnNameToLabel(column)}: </span>{card[column]}</p>))
                         }
                     </div>);
                 })}
