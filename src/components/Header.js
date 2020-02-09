a20cea22a1d57161838271040ec8f6952674e493
haydeemunoz <haydeemunozdelarocha@gmail.com>
2020-02-09 21:50:53 +0100

    fixinf autorizado and confirm

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../images/maspost-sm.png';
 import ResponsiveMenu from "./ResponsiveMenu";
+import {isMobileMedium} from "../helpers/responsive";
 
 function Header(props) {
     const {isLoggedIn, isSuperAdmin} = props;
+    const isMobile = isMobileMedium();
+
     return (
-        <header className="main-header">
+        <header className={`main-header ${isMobile ? 'mobile-header' : ''}`}>
             <nav className="navbar navbar-default " role="navigation">
                 <div className="navbar-header">
                     <a href="/">
