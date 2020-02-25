6392479359c39f71c128fceccd481662debc4d75
haydeemunoz <haydeemunozdelarocha@gmail.com>
2020-02-25 17:50:54 +0100

    addbrowseralert

--- src/index.js
+++ src/index.js
@@ -11,13 +11,15 @@ import { CookiesProvider } from 'react-cookie';
 
 Sentry.init({dsn: "https://3023cd63d3aa45af8b8b9b325f18dbe3@sentry.io/2357811",  environment: process.env.NODE_ENV});
 
-if (!window.Promise) {
-    Sentry.captureException(new Error('Promise not defined'));
-    alert('Hubo un error al cargar la página de Maspost Warehouse. Nuestra aplicación no es compatible con la versión de tu navegador, para accesar la página actualiza tu navegador. Navegadores Compatibles: IE  9  y 10, Chrome, Safari, Firefox,  Edge');
-    window.location.href = 'https://www.whatismybrowser.com/guides/how-to-update-your-browser/';
+try {
+    ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('root'));
+}  catch(e) {
+    if (!window.Promise) {
+        Sentry.captureException(e);
+        alert('Hubo un error al cargar la página de Maspost Warehouse. Nuestra aplicación no es compatible con la versión de tu navegador, para accesar la página actualiza tu navegador. Navegadores Compatibles: IE  9  y 10, Chrome, Safari, Firefox,  Edge');
+        window.location.href = 'https://www.whatismybrowser.com/guides/how-to-update-your-browser/';
+    }
 }
-
-ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
