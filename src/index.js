b27c783f09ed18a3d758db5291a4959b26fd0f0e
haydeemunoz <haydeemunozdelarocha@gmail.com>
2020-02-25 17:51:10 +0100

    addbrowseralert

--- src/index.js
+++ src/index.js
@@ -14,11 +14,9 @@ Sentry.init({dsn: "https://3023cd63d3aa45af8b8b9b325f18dbe3@sentry.io/2357811", 
 try {
     ReactDOM.render(<CookiesProvider><App /></CookiesProvider>, document.getElementById('root'));
 }  catch(e) {
-    if (!window.Promise) {
-        Sentry.captureException(e);
-        alert('Hubo un error al cargar la página de Maspost Warehouse. Nuestra aplicación no es compatible con la versión de tu navegador, para accesar la página actualiza tu navegador. Navegadores Compatibles: IE  9  y 10, Chrome, Safari, Firefox,  Edge');
-        window.location.href = 'https://www.whatismybrowser.com/guides/how-to-update-your-browser/';
-    }
+    Sentry.captureException(e);
+    alert('Hubo un error al cargar la página de Maspost Warehouse. Nuestra aplicación no es compatible con la versión de tu navegador, para accesar la página actualiza tu navegador. Navegadores Compatibles: IE  9  y 10, Chrome, Safari, Firefox,  Edge');
+    window.location.href = 'https://www.whatismybrowser.com/guides/how-to-update-your-browser/';
 }
 
 // If you want your app to work offline and load faster, you can change
